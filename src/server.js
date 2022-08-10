b7e75553dda1f0a56d3a69feeda312735e5ebf86
Yousaf Nabi <yousaf@pactflow.io>
2022-08-10 12:06:42 +0100

    chore: update routes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const routes = require('./src/product/product.routes');
+const cors = require("cors");
+const routes = require("./product/product.routes");
 
 const port = 3001;
 
 const init = () => {
-    app.use(express.json());
-    app.use(cors());
-    app.use(routes);
-    return app.listen(port, () => console.log(`Provider API listening on port ${port}...`));
+  app.use(express.json());
+  app.use(cors());
+  app.use(routes);
+  return app.listen(port, () =>
+    console.log(`Provider API listening on port ${port}...`)
+  );
 };
 
 init();
